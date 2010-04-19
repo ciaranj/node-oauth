2bf33d156be5b41f38de6e031d3cfd6e6ca5fcb7
ciaranj <ciaranj@gmail.com>
2010-04-19 22:02:36 +0100

    Added a method to access protected resources.

---
+++ examples/term.ie.oauth.js
@@ -0,0 +1,37 @@
+var sys= require('sys')
+
+var OAuth= require('../lib/oauth').OAuth;
+
+var oa= new OAuth("http://term.ie/oauth/example/request_token.php",
+                  "http://term.ie/oauth/example/access_token.php",
+                  null,
+                  "key",
+                  "secret",
+                  "1.0",
+                  "HMAC-SHA1")
+
+oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, authorize_url,  results){
+  if(error) sys.puts('error :' + error)
+  else { 
+    sys.puts('oauth_token :' + oauth_token)
+    sys.puts('oauth_token_secret :' + oauth_token_secret)
+    sys.puts('requestoken results :' + sys.inspect(results))
+    sys.puts("Requesting access token")
+    oa.getOauthAccessToken(oauth_token, oauth_token_secret, function(error, oauth_access_token, oauth_access_token_secret, results2) {
+      sys.puts('oauth_access_token :' + oauth_access_token)
+      sys.puts('oauth_token_secret :' + oauth_access_token_secret)
+      sys.puts('accesstoken results :' + sys.inspect(results2))
+      sys.puts("Requesting access token")
+      var data= "";
+      oa.getProtectedResource(oauth_access_token, oauth_access_token_secret, "http://term.ie/oauth/example/echo_api.php?foo=bar&too=roo", function (response) {
+        response.setEncoding('utf8');
+        response.addListener('data', function (chunk) {
+          data+=chunk;
+        });
+        response.addListener('end', function () {
+          sys.puts(response.statusCode + " : " + data);
+        });
+      });
+    });
+  }
+})
