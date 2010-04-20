36eded6a2ec7410b350b919fa8fe9eccee22e5ca
ciaranj <ciaranj@gmail.com>
2010-04-20 22:37:28 +0100

    Added in support for PLAINTEXT Signature methods.

---
+++ examples/term.ie.oauth-PLAINTEXT.js
@@ -0,0 +1,31 @@
+var sys= require('sys')
+
+var OAuth= require('../lib/oauth').OAuth;
+
+var oa= new OAuth("http://term.ie/oauth/example/request_token.php?foo=bar",
+                  "http://term.ie/oauth/example/access_token.php",
+                  null,
+                  "key",
+                  "secret",
+                  "1.0",
+                  "PLAINTEXT")
+
+oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, authorize_url,  results){
+  if(error) sys.puts('error :' + error)
+  else { 
+    sys.puts('oauth_token :' + oauth_token)
+    sys.puts('oauth_token_secret :' + oauth_token_secret)
+    sys.puts('requestoken results :' + sys.inspect(results))
+    sys.puts("Requesting access token")
+    oa.getOauthAccessToken(oauth_token, oauth_token_secret, function(error, oauth_access_token, oauth_access_token_secret, results2) {
+      sys.puts('oauth_access_token :' + oauth_access_token)
+      sys.puts('oauth_token_secret :' + oauth_access_token_secret)
+      sys.puts('accesstoken results :' + sys.inspect(results2))
+      sys.puts("Requesting access token")
+      var data= "";
+      oa.getProtectedResource("http://term.ie/oauth/example/echo_api.php?foo=bar&too=roo", "GET", oauth_access_token, oauth_access_token_secret,  function (error, data, response) {
+          sys.puts(data);
+      });
+    });
+  }
+})
