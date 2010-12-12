2c0b8c3b795a35fd3af0084240133d62dca0ac2d
ciaranj <ciaranj@gmail.com>
2010-12-12 22:38:56 +0000

    Fixes issue 17 - Handles rev07 and above of the OAuth spec

---
+++ tests/oauth2.js
@@ -0,0 +1,27 @@
+var vows = require('vows'),
+    assert = require('assert'),
+    OAuth2= require('../lib/oauth2').OAuth2;
+
+vows.describe('OAuth2').addBatch({
+    'When handling the access token response': {
+        topic: new OAuth2(),
+        'we should correctly extract the token if received as form-data': function (oa) {
+            oa._request= function( method, url, fo, bar, callback) {
+              callback(null, "access_token=access&refresh_token=refresh");
+            };
+            oa.getOAuthAccessToken("", {}, function(error, access_token, refresh_token) {
+              assert.equal( access_token, "access");
+              assert.equal( refresh_token, "refresh");
+            });
+        },
+        'we should correctly extract the token if received as a JSON literal': function (oa) {
+          oa._request= function( method, url, fo, bar, callback) {
+            callback(null, '{"access_token":"access","refresh_token":"refresh"}');
+          };
+          oa.getOAuthAccessToken("", {}, function(error, access_token, refresh_token) {
+            assert.equal( access_token, "access");
+            assert.equal( refresh_token, "refresh");
+          });
+        }
+    }
+}).export(module);
