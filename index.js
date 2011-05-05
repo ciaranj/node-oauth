0158a78c4c78a498aaeb9d1cfdcf24c3ea91a53c
meltingice <meltingice8917@gmail.com>
2011-05-05 10:56:23 -0400

    Added support for OAuth Echo

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-exports.OAuth = require("./lib/oauth").OAuth;  
+exports.OAuth = require("./lib/oauth").OAuth;
+exports.OAuthEcho = require("./lib/oauthecho").OAuthEcho;
 exports.OAuth2 = require("./lib/oauth2").OAuth2;
