b0c2019a42f1f55a49e99adf7a20ef618b3701f1
darul <darul75@gmail.com>
2014-02-22 12:34:37 +0100

    Revert 24597c4..70ba087
    This rolls back to commit 24597c4701435e1af0ee83cbee209f731ef4def4.

--- tests/oauth-tunnel.js
+++ tests/oauth-tunnel.js
@@ -1,43 +1,8 @@
-describe('tunne with oauth',function() {
-  var http = require('http');
-  var net = require('net');
+describe('OAuth1.0',function(){
   var OAuth= require('../lib/oauth');
   var OAuth2= require('../lib/oauth2');
 
-
-  // CREATE PROXY
-  before(function(done){    
-
-    var server = http.createServer(function(request, response) {
-
-    }).listen(8081);
-
-    server.on('listening', function() {
-        console.log('proxy started');
-        done();
-    });
-
-    server.on('connect', onConnect); // for v0.7 or later
-
-    // TUNNELING
-    function onConnect(req, clientSocket, head) {
-      console.log('PROXY: got CONNECT request');
-      console.log('PROXY: creating a tunnel');
-      var serverSocket = net.connect(443, 'api.twitter.com', function() {
-        console.log('PROXY: replying to client CONNECT request');
-        clientSocket.write('HTTP/1.1 200 Connection established\r\n\r\n');
-        clientSocket.pipe(serverSocket);
-        serverSocket.write(head);
-        serverSocket.pipe(clientSocket);
-        // workaround, see joyent/node#2524
-        serverSocket.on('end', function() {
-          clientSocket.end();
-        });
-      });
-    }
-  });
-
-  it('oauth1 trends Twitter API v1.1',function(done) {    
+  it('tests trends Twitter API v1.1',function(done){
     var twitterConsumerKey = '';
     var twitterConsumerSecret = '';
     var twitterAccessToken = '';
@@ -56,7 +21,7 @@ describe('tunne with oauth',function() {
       {
         proxy: {
             host: '127.0.0.1', // PUT PROXY ADDRESS
-            port: 8081
+            port: 8080
         }
       }
     );
@@ -71,8 +36,7 @@ describe('tunne with oauth',function() {
       });    
   });
 
-  it('oauth2 gets bearer token', function(done) {
-
+  it('gets bearer token', function(done){    
      var twitterConsumerKey = '';
      var twitterConsumerSecret = '';
      var oauth2 = new OAuth2.OAuth2(twitterConsumerKey, twitterConsumerSecret, 
@@ -83,7 +47,7 @@ describe('tunne with oauth',function() {
        {
           proxy: {
               host: '127.0.0.1', // your proxy address
-              port: 8081
+              port: 8080
           }
         }
        );
@@ -94,11 +58,6 @@ describe('tunne with oauth',function() {
        console.log('bearer: ',access_token);
        done();
      });
-
-      this.timeout(200000);
-
-
-  
    });
 
 });   
