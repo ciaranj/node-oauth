202ec07a04729cff26147fb75604147fcb6f0294
Rudolf Olah <omouse@gmail.com>
2016-03-20 12:53:18 -0400

    OAuth2 Promises Support
    Added bluebird as a dependency
    
    Using Bluebird's promisifyAll and an object that delegates methods to
    the OAuth2 implementation to be able to support promises.
    
    Copied tests from oauth2 to oauth2promises. Existing tests are passing
    with some small corrections.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 exports.OAuth = require("./lib/oauth").OAuth;
 exports.OAuthEcho = require("./lib/oauth").OAuthEcho;
 exports.OAuth2 = require("./lib/oauth2").OAuth2;
+exports.PromiseOAuth2 = require("./lib/oauth2-promise").OAuth2;
