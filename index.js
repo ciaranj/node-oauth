bff7e8bb1cd6e31cf20b34a2a7c259cf7644bbfd
Rudolf Olah <omouse@gmail.com>
2016-03-25 19:37:14 -0400

    work in progress, OAuth 1 promises support

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 exports.OAuth = require("./lib/oauth").OAuth;
 exports.OAuthEcho = require("./lib/oauth").OAuthEcho;
 exports.OAuth2 = require("./lib/oauth2").OAuth2;
+exports.PromiseOAuth = require("./lib/oauth-promise").OAuth;
 exports.PromiseOAuth2 = require("./lib/oauth2-promise").OAuth2;
