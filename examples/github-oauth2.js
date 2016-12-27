bb7e53f48446e39fe7bd669439fe206067dd5a58
Rudolf Olah <omouse@gmail.com>
2016-12-26 19:42:47 -0500

    code cleanup #8

--- examples/github-oauth2.js
+++ examples/github-oauth2.js
@@ -17,37 +17,37 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-var OAuth2 = require('../index').PromiseOAuth2;
+var OAuth2 = require("../index").PromiseOAuth2;
 
-var clientId = '';
-var clientSecret = '';
+var clientId = "";
+var clientSecret = "";
 
 // Fill these in:
-var user = 'USER';
-var personalAccessToken = 'PERSONAL_ACCESS_TOKEN';
+var user = "USER";
+var personalAccessToken = "PERSONAL_ACCESS_TOKEN";
 
-var baseSiteUrl = 'https://' + user + ':' + personalAccessToken + '@api.github.com/';
-var authorizePath = 'oauth2/authorize';
-var accessTokenPath = 'oauth2/access_token';
+var baseSiteUrl = "https://" + user + ":" + personalAccessToken + "@api.github.com/";
+var authorizePath = "oauth2/authorize";
+var accessTokenPath = "oauth2/access_token";
 var customHeaders = null;
+
+function jsonParse(data) {
+  return JSON.parse(data);
+}
 
 var oauth2 = new OAuth2(
   clientId, clientSecret, baseSiteUrl, authorizePath, accessTokenPath, customHeaders
 );
 
-var url = 'https://api.github.com/users/' + user + '/received_events';
+var url = "https://api.github.com/users/" + user + "/received_events";
 oauth2
   .get(url, personalAccessToken)
   .then(jsonParse)
   .then(function(json) {
     for (var i = 0; i < json.length; i += 1) {
-      console.log(json[i]['id'] + ': ' + json[i].type);
+      console.log(json[i]["id"] + ": " + json[i].type);
     }
   })
   .catch(function(err) {
-    console.log('Error: ' + err);
+    console.log("Error: " + err);
   });
-
-function jsonParse(data) {
-  return JSON.parse(data);
-}
